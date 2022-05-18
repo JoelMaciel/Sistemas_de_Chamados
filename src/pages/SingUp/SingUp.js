6e86e1b9d282290db2ed70f0477e8ace54c4423e
Joel Maciel <soldado25461@gmail.com>
2022-05-18 14:26:58 -0300

    error route

---
+++ src/pages/SingUp/SingUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingUp = () => {
+  return (
+    <div>Tela de cadastro</div>
+  )
+}
+
+export default SingUp
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { Switch } from "react-router-dom";
+import Route from "./Route";
+import SingIn from "../pages/SingIn";
+import SingUp from "../pages/SingIn";
+import Dashboard from "../pages/Dashboard";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={SingIn} />
+      <Route exact path="/register" component={SingUp} />
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
+    </Switch>
+  );
+}
